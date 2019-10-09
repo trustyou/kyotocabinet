d33cafc9969a54675bb2c579fa8595dd82b876a0
Yuriy Davygora <yuriy.davygora@trustyou.net>
2019-10-09 12:23:05 +0200

    Updating copyright

--- kcutil.cc
+++ kcutil.cc
@@ -1,6 +1,8 @@
 /*************************************************************************************************
  * Utility functions
  *                                                               Copyright (C) 2009-2012 FAL Labs
+ *                                                               Copyright (C) 2013-2017 Cloudflare Inc.
+ *                                                               Copyright (C) 2018-2019 TrustYou GmbH
  * This file is part of Kyoto Cabinet.
  * This program is free software: you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation, either version
